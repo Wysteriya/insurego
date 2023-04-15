4734c650cdeed81470cf6b893423b70ce8aa59e1
hari134 <mailtohari134@gmail.com>
2023-04-15 17:20:45 +0530

    added dockerfile

--- gpp/models/buy_ins.go
+++ gpp/models/buy_ins.go
@@ -3,6 +3,5 @@ package models
 type ReceiveBuyIns struct {
 	PrivateKey  string `json:"private_key"`
 	PublicKey   string `json:"public_key"`
-	UserId      string `json:"user_id"`
 	PolicyRefId string `json:"policy_ref_id"`
 }
